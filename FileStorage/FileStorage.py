939bfcc2e4e3f8e675ad3c6130505e674532dadf
Tobiasz <94205675+Swam-pl@users.noreply.github.com>
2024-07-05 22:24:09 +0200

    updates

--- FileStorage/FileStorage.py
+++ FileStorage/FileStorage.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, request
+from flask import Flask, render_template, send_from_directory, request, abort
 from werkzeug import utils
 import os
 
@@ -17,21 +17,25 @@ def upload_file():
     if(request.method=='POST'):
         file=request.files['file']
         filename=file.filename
+        if(filename==''):
+            return render_template('file_upload.html', status="You haven't attached any files!",saved=False)
         if(filename not in get_file_list()):
             file.save('files/'+utils.secure_filename(filename))
-            return render_template('file_upload.html', saved=True)
-        return render_template('file_upload.html', saved=False)
+            return render_template('file_upload.html', status='file has been saved on the server.',saved=True)
+        return render_template('file_upload.html', status='cannot save file - file with such name already exists.',saved=False)
     return render_template('file_upload.html')
 
 @app.route('/download/<file>')
 def send_file(file):
     if(os.path.isfile(f"files/{file}")):
         return send_from_directory('files', file, as_attachment=True)
+    else:
+        abort(404)
 
 @app.route('/download')
 def download():
     files=get_file_list()
-    return render_template("file_download.html", files=files)
+    return render_template("file_download.html", files=files, number_of_files=len(files))
 
 @app.route('/')
 def index():
