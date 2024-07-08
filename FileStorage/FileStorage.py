52ee501320629e6aac4403ee2cde38ff99c8d128
Tobiasz <94205675+Swam-pl@users.noreply.github.com>
2024-07-08 19:39:26 +0200

    updates

--- FileStorage/FileStorage.py
+++ FileStorage/FileStorage.py
@@ -2,11 +2,15 @@ from flask import Flask, render_template, send_from_directory, request, abort
 from werkzeug import utils
 import os
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 def get_file_list():
-    files=tuple(x for x in os.listdir('files') if os.path.isfile(f'files/{x}'))
+    files = dict((x, os.path.getsize(f'files/{x}')) for x in os.listdir('files') if os.path.isfile(f'files/{x}'))
     return files
+
+@app.route('/favicon.ico')
+def send_favicon():
+    return send_from_directory('static', 'favicon.ico')
 
 @app.route('/style.css')
 def send_css():
@@ -14,28 +18,28 @@ def send_css():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    if(request.method=='POST'):
-        file=request.files['file']
-        filename=utils.secure_filename(file.filename)
-        if(filename==''):
-            return render_template('file_upload.html', status="You haven't attached any files!",saved=False)
+    if(request.method == 'POST'):
+        file = request.files['file']
+        filename = utils.secure_filename(file.filename)
+        if(filename == ''):
+            return render_template('file_upload.html', status = "You haven't attached any files!",saved = False)
         elif(filename not in get_file_list()):
-            file.save('files/'+filename)
-            return render_template('file_upload.html', status='File has been saved on the server.',saved=True)
-        return render_template('file_upload.html', status='Cannot save file - file with such name already exists.',saved=False)
+            file.save('files/' + filename)
+            return render_template('file_upload.html', status = 'File has been saved on the server.', saved = True)
+        return render_template('file_upload.html', status = 'Cannot save file - file with such name already exists.', saved = False)
     return render_template('file_upload.html')
 
 @app.route('/download/<file>')
 def send_file(file):
     if(os.path.isfile(f"files/{file}")):
-        return send_from_directory('files', file, as_attachment=True)
+        return send_from_directory('files', file, as_attachment = True)
     else:
         abort(404)
 
 @app.route('/download')
 def download():
     files=get_file_list()
-    return render_template("file_download.html", files=files, number_of_files=len(files))
+    return render_template("file_download.html", files = files, number_of_files = len(files))
 
 @app.route('/')
 def index():
