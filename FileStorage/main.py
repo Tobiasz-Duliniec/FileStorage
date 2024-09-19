9401c5939297c3cf8ece8be863b640162f9803d0
Tobiasz Duliniec <94205675+Tobiasz-Duliniec@users.noreply.github.com>
2024-09-19 02:25:39 +0200

    Update main.py

--- FileStorage/main.py
+++ FileStorage/main.py
@@ -115,7 +115,7 @@ def logout():
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-if(__name__=="__main__"):
+if(__name__ == '__main__'):
     app.run()
